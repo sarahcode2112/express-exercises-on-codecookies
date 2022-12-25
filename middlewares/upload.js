83074f91e70796d0e888369b32cbebd69763d15b
Sarah Warren <sarah.warren@code.berlin>
2022-12-26 00:24:24 +0100

    deleted unused files and comments

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,12 +1,12 @@
 import util from "util"
 import Multer from "multer"
-const maxSize = 5 * 1024 * 1024 // 5mb
+// 5mb max upload size:
+const maxSize = 5 * 1024 * 1024 
 
 let processFile = Multer({
     storage: Multer.memoryStorage(),
     limits: { fileSize: maxSize }
 }).single("audiofile.mp3")
 
-// I hope this works. I don't quite remember how to export correctly. Changed it from the tutorial code:
 export let processFileMiddleware = util.promisify(processFile)
 
