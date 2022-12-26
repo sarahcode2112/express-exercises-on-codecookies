9283ff77054a4ba69b4211434faf073a7aa80e20
Sarah Warren <sarah.warren@code.berlin>
2022-12-26 18:39:16 +0100

    remove commented-out code from user.js

--- user.js
+++ user.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose'
 import passportLocalMongoose from 'passport-local-mongoose'
 
-// mongoose.connect('mongodb://localhost/cookieshop', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-
 const Schema = mongoose.Schema
 
 const User = new Schema({
